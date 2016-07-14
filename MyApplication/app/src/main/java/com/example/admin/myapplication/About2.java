cdf714c13d65f3c059f2d9aaf5d4f742876d00b9
wannisa <aoy.wannisa1994@gmail.com>
2016-07-14 21:51:13 +0700

    NewProjectSenate

---
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/About2.java
@@ -0,0 +1,35 @@
+package com.example.admin.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Created by Admin on 6/7/2559.
+ */
+public class About2 extends Activity {
+    private Button blak;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        blak = (Button)findViewById(R.id.back);
+
+        Intent intent = this.getIntent();
+        final String ID = intent.getStringExtra("ID");
+        final int id = intent.getExtras().getInt("id");
+        blak.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), detailactivity2.class);
+                intent.putExtra("id", id);
+                intent.putExtra("ID", ID);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
